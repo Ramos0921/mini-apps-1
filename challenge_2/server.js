88c15af1b340613b0e0542757965782c81b287e4
Ramos0921 <sfcc.eduardo.ramos@gmail.com>
2020-06-09 15:43:55 -0700

    creating csv doc with proper data

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs')
+var path = require('path');
 
 var app = express();
 
@@ -9,36 +10,66 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname+'/client'));
 
 app.set('port',3000)
-app.listen(app.get('port'),()=>{
-  console.log('listening on port', app.get('port'))
-})
+
 
 app.get('/', (req,res)=>{
 
 
 })
 app.post('/text',(req,res)=>{
+  fs.open(path.join(__dirname +'data.csv'),'w',()=>{
+    var text= req.body.textArea;
 
-  fs.writeFile('result.txt', makeJson(req.body.textArea),(err)=>{
-    if(err){
-      throw err;
+
+    if(text[text.length-1] === ';'){
+      text = text.slice(0,-1);
+
+    };
+    var data = JSON.parse(text);
+    var header = '';
+    for(key in data){
+      if(key !== 'children'){
+        header = header +key +',';
+      };
     }
-    //res.download('result.txt')
+    header = header.slice(0,-1)+'\n';
+    //takes a path and text and callback
+    fs.appendFile('data.csv', header,()=>{
+      var totalRows = '';
+      var recurse = function(data){
+        //base case
+        if(!data.children){
+          return;
+        }
+        var row = '';
+
+        for(var key in data){
+          if(key !== "children"){
+            row = row+ data[key]+',';
+          }
+        }
+
+        row = row.slice(0,-1)+'\n';
+        totalRows = totalRows+row;
+        for(var i =0; i<data.children.length;i++){
+          recurse(data.children[i]);
+        };
+      }
+
+      recurse(data);
+      console.log(totalRows);
+      fs.appendFile('data.csv',totalRows,()=>{
+        fs.readFile(path.join(__dirname+'data.csv'),(err,data)=>{
+          res.send(data);
+        })
+      })
+    })
+
   })
 })
 
-var makeJson= function(string){
-  var stringSplit = string.split(';')[0];
-  var arr = [];
-
-  stringSplit = JSON.parse(stringSplit);
-
-  var recurse = function(obj){
-    var result =[];
-    result.push(Object.values(obj).slice(0,-1))
-    console.log(result)
-  }
-  recurse(stringSplit)
-}
+app.listen(app.get('port'),()=>{
+  console.log('listening on port', app.get('port'))
+})
 
 module.exports.app = app;
